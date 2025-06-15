fbc7688a25eb2bb6123ea982714762e19c1dda8b
janab957 <janabarrani75@gmail.com>
2025-06-15 11:42:19 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('libra-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/librarianDashboard_responsive.html',
+        '/notifications.html',
+        '/overallCSS_responsive.css',
+        '/librarianDashboardJscript_responsive.js',
+        '/manifest.json',
+        '/images/icons/icon-192x192.png',
+        '/images/icons/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
